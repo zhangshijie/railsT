0ff420aa362ea0115a563c4a9a09612ce4cb34d1
CJ <cjzhang88@gmail.com>
2017-04-27 21:10:13 +0800

    具名路由

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  get '/help' , to: 'static_pages#help'
   
-  get 'static_pages/about'
+  get '/about' , to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
