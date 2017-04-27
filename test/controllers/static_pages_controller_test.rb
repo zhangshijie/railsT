c30cdc26bea9ab6fcde2cb70e9f40039ef512328
CJ <cjzhang88@gmail.com>
2017-04-27 15:03:48 +0800

    title使用helper类里的方法

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Turorail Sample App"
+    @base_title = "Ruby on Rails Tutorail Sample App"
   end
   
   test "should get root" do
@@ -15,7 +15,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title" , "Home | #{@base_title}"
+    assert_select "title" , "Ruby on Rails Tutorail Sample App"
   end
 
   test "should get help" do
