7ba869a45206d218c8cb573e3f3723eeae01c0cf
CJ <cjzhang88@gmail.com>
2017-04-28 17:40:36 +0800

    用户查询页，以及将usercontroller,userhelper为复数命名

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'user/new'
-
   root 'static_pages#home'
-
-  get 'static_pages/home'
 
   get '/help' , to: 'static_pages#help'
   
@@ -14,5 +10,6 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'user#new'
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
