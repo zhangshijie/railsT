653ffad02340eed47aa5955d288b647eaeeaa61a
CJ <cjzhang88@gmail.com>
2017-05-04 18:37:44 +0800

    退出登录

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,29 +2,34 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "valid signup information"  do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path , params: { user: { name: "",
-                                          email: "user@invalid",
-                                          password: "foo",
-                                          password_confirmation: "bar"}}
-      end
-      assert_template 'users/new'
-      assert_select 'div#<CSS id for error explanation>'
-      assert_select 'div.<CSS class for field with errors>'
-    end 
-  end
+  # test "valid signup information"  do
+  #   get signup_path
+  #   assert_no_difference 'User.count' do
+  #     post users_path , params: { user: { name: "",
+  #                                         email: "user@invalid",
+  #                                         password: "foo",
+  #                                         password_confirmation: "bar"}}
 
-  test "valid signup information"  do 
-    get signup_path
-    assert_differrence 'User.count', 1 do
-      post users_path ,params: {user: { name: "",
-                                          email: "user@invalid",
-                                          password: "foo",
-                                          password_confirmation: "bar"}}
+  #   end
+  #   assert_template 'users/new'
+  #   assert_select 'div#<CSS id for error explanation>'
+  #   assert_select 'div.<CSS class for field with errors>'
+  # end 
+
+
+  test "valid signup information" do
+    get signup_path 
+    assert_difference 'User.count', 1 do 
+      post users_path , params: { user: {
+                                          name: "Exmaple User", 
+                                          email: "user@example.com",
+                                          password: "password",
+                                          password_confirmation: "password" 
+                                        }
+                                }
     end
     follow_redirect!
     assert_template 'users/show'
-  end 
+    assert is_logged_in?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,11 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
+
+  #在测试中定义检查登陆状态的方法，返回布尔值
+  #为啥要在这里定义这个方法
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
   
 end
