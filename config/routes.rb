be49cf652d7ff81788245dd3eba3aaa8f346750c
CJ <cjzhang88@gmail.com>
2017-05-04 17:07:02 +0800

    登陆以及登陆的测试

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
