8d67bc4c2bb861203296a5c9527ea9a91a98c3bf
CJ <cjzhang88@gmail.com>
2017-05-07 22:12:51 +0800

    更新用户资料

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  # test "unsuccessful edit" do
+  #   get edit_user_path(@user)
+  #   assert_template 'users/edit'
+  #   patch user_path(@user), params: {user: {name: "",
+  #                                           email: "foo@invalid",
+  #                                           password: "foo",
+  #                                           password_confirmation: "bar"}}
+
+  #   assert_template 'user/edit'
+  # end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name:  "",
+                                              email: "foo@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+    assert_template 'users/edit'
+  end
+end
