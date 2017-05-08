c05db5f20efcf2583d9c94fbcac93d11ad79ecd9
CJ <cjzhang88@gmail.com>
2017-05-08 11:43:45 +0800

    显示所有用户

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
@@ -32,6 +40,17 @@ class UsersController < ApplicationController
     end
   end
 
+   def logged_in_user
+      unless logged_in?
+        # byebug
+        store_location
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      else
+        # byebug
+      end
+    end
+
   private
 
   # def user_params
@@ -40,5 +59,10 @@ class UsersController < ApplicationController
     def user_params
           params.require(:user).permit(:name, :email, :password,
                                     :password_confirmation)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?
     end
 end
