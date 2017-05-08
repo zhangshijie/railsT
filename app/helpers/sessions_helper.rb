fecaea66f9bed75ce3422d003c8a99aaa5d88bb3
CJ <cjzhang88@gmail.com>
2017-05-08 11:43:53 +0800

    权限系统

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -39,5 +43,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+# 重定向到存储的地址或默认地址
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  #存储后面需要使用的地址
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,16 +5,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  # test "unsuccessful edit" do
-  #   get edit_user_path(@user)
-  #   assert_template 'users/edit'
-  #   patch user_path(@user), params: {user: {name: "",
-  #                                           email: "foo@invalid",
-  #                                           password: "foo",
-  #                                           password_confirmation: "bar"}}
-
-  #   assert_template 'user/edit'
-  # end
 
   test "unsuccessful edit" do
     log_in_as(@user)
@@ -25,5 +15,31 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password:              "foo",
                                               password_confirmation: "bar" } }
     assert_template 'users/edit'
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user) , params: {user: {name: @user.name, email: @user.email}}
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "successful edit with friendly forwarding" do 
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: {name: name, email: email, password: password, password_confirmation: password_confirmation}}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name , @user.name
+    assert_equal email, @user.email
   end
 end
