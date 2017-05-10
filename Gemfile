97a5b5186c4092771d75dddafbba064c3b9a5568
CJ <cjzhang88@gmail.com>
2017-05-10 16:26:06 +0800

    上传微博图片

--- Gemfile
+++ Gemfile
@@ -37,9 +37,9 @@ gem 'faker', '1.6.6'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate', '0.0.10'
 
-# gem 'carrierwave', '0.11.2'
-# gem 'mini_magick','4.5.1'
-# gem 'fog',  '1.38.0'
+gem 'carrierwave', '0.11.2'
+gem 'mini_magick','4.5.1'
+gem 'fog',  '1.38.0'
 
 
 
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,16 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
   default_scope -> { order(created_at: :desc)}
+  validate :picture_size
+  mount_uploader :picture, PictureUploader
 
+
+  private
+  
+    # 验证上传的图像大小
+    def picture_size
+      if picture.size > 5.megabytes
+        error.add(:picture, "should be less than 5MB")
+      end
+    end
 end
